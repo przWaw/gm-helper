a8f9d30f873ed2100b14bb32321e734d6adf1e03
pWaw <przemko_wawrzyniak@wp.pl>
2024-02-23 18:29:01 +0100

    Init

---
+++ src/main/java/com/pWaw/mghelper/MgHelperApplication.java
@@ -0,0 +1,13 @@
+package com.pWaw.mghelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgHelperApplication.class, args);
+	}
+
+}
